491d6aa5a95c3f328dd18fa9ed45f8b422566dbe
NikhilMamilla <23211a05m7@bvrit.ac.in>
2025-12-08 10:35:48 +0530

    Changes done new

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,8 +18,6 @@ const Navbar: React.FC = () => {
     ? `${baseNavLinkClasses} text-white/90 hover:text-white hover:border-cyan-400 hover:text-[1rem]`
     : `${baseNavLinkClasses} text-[#222] hover:text-primary-blue hover:border-primary-blue hover:text-[1rem]`;
 
-
-
   return (
     <header className={`w-full z-[1000] transition-all duration-300 ${isTransparentPage ? 'absolute top-0 left-0 bg-transparent' : 'relative bg-white shadow-[0_2px_5px_rgba(0,0,0,0.1)]'}`}>
       <nav className={`font-playfair relative max-w-[1200px] mx-auto px-8 py-4 flex justify-between items-center h-[72px] ${isTransparentPage ? 'bg-transparent' : 'bg-white'}`}>
@@ -36,9 +34,9 @@ const Navbar: React.FC = () => {
           className="flex flex-col gap-[6px] cursor-pointer md:hidden"
           onClick={toggleMenu}
         >
-          <span className={`w-[25px] h-[3px] rounded-[3px] transition-all duration-300 ${isTransparentPage ? 'bg-white' : 'bg-black'}`}></span>
-          <span className={`w-[25px] h-[3px] rounded-[3px] transition-all duration-300 ${isTransparentPage ? 'bg-white' : 'bg-black'}`}></span>
-          <span className={`w-[25px] h-[3px] rounded-[3px] transition-all duration-300 ${isTransparentPage ? 'bg-white' : 'bg-black'}`}></span>
+          <span className={`w-[25px] h-[3px] rounded-[3px] transition-all duration-300 ${isTransparentPage ? 'bg-white' : 'bg-black'} ${menuOpen ? 'rotate-45 translate-y-[9px]' : ''}`}></span>
+          <span className={`w-[25px] h-[3px] rounded-[3px] transition-all duration-300 ${isTransparentPage ? 'bg-white' : 'bg-black'} ${menuOpen ? 'opacity-0' : 'opacity-100'}`}></span>
+          <span className={`w-[25px] h-[3px] rounded-[3px] transition-all duration-300 ${isTransparentPage ? 'bg-white' : 'bg-black'} ${menuOpen ? '-rotate-45 -translate-y-[9px]' : ''}`}></span>
         </div>
 
         {/* Nav Links */}
@@ -47,27 +45,27 @@ const Navbar: React.FC = () => {
           ${menuOpen ? 'flex flex-col absolute top-full right-0 w-full bg-white p-[1.2rem] shadow-md' : 'hidden md:flex'}
         `}>
           <li className="group relative">
-            <Link to="/" className={menuOpen ? navLinkClasses.replace('text-white/90', 'text-[#222]') : navLinkClasses}>HOME</Link>
+            <Link to="/" onClick={() => setMenuOpen(false)} className={menuOpen ? navLinkClasses.replace('text-white/90', 'text-[#222]') : navLinkClasses}>HOME</Link>
           </li>
 
           <li className="group relative">
-            <Link to="/facilities" className={menuOpen ? navLinkClasses.replace('text-white/90', 'text-[#222]') : navLinkClasses}>FACILITIES</Link>
+            <Link to="/facilities" onClick={() => setMenuOpen(false)} className={menuOpen ? navLinkClasses.replace('text-white/90', 'text-[#222]') : navLinkClasses}>FACILITIES</Link>
           </li>
 
           <li className="group relative">
-            <Link to="/startups" className={menuOpen ? navLinkClasses.replace('text-white/90', 'text-[#222]') : navLinkClasses}>STARTUPS</Link>
+            <Link to="/startups" onClick={() => setMenuOpen(false)} className={menuOpen ? navLinkClasses.replace('text-white/90', 'text-[#222]') : navLinkClasses}>STARTUPS</Link>
           </li>
 
           <li className="group relative">
-            <Link to="/team" className={menuOpen ? navLinkClasses.replace('text-white/90', 'text-[#222]') : navLinkClasses}>OUR TEAM</Link>
+            <Link to="/team" onClick={() => setMenuOpen(false)} className={menuOpen ? navLinkClasses.replace('text-white/90', 'text-[#222]') : navLinkClasses}>OUR TEAM</Link>
           </li>
 
           <li className="group relative">
-            <Link to="/events" className={menuOpen ? navLinkClasses.replace('text-white/90', 'text-[#222]') : navLinkClasses}>EVENTS</Link>
+            <Link to="/events" onClick={() => setMenuOpen(false)} className={menuOpen ? navLinkClasses.replace('text-white/90', 'text-[#222]') : navLinkClasses}>EVENTS</Link>
           </li>
 
           <li className="group relative">
-            <Link to="/contact" className={menuOpen ? navLinkClasses.replace('text-white/90', 'text-[#222]') : navLinkClasses}>CONTACT</Link>
+            <Link to="/contact" onClick={() => setMenuOpen(false)} className={menuOpen ? navLinkClasses.replace('text-white/90', 'text-[#222]') : navLinkClasses}>CONTACT</Link>
           </li>
         </ul>
       </nav>
